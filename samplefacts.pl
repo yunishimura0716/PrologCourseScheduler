29579e38e5ef2886f79c972d083113443c4f048e
james-magee <jam020201@gmail.com>
2023-04-02 17:14:32 -0700

    sample facts

---
+++ samplefacts.pl
@@ -0,0 +1,194 @@
+section(null, undefined, Lecture, 1, Hybrid                        ,  Mon, 14:30, 16:30, undefined).
+section(null, undefined, Lecture, 1, Hybrid                        ,  Wed, 14:30, 15:30, undefined).
+section(null, undefined, Lecture, 1, Multi-access                  ,  Tue Thu, 15:30, 17:00, undefined).
+section(null, undefined, Waiting List, 1, In-Person,  Tue Thu,  ,  , undefined).
+section(null, undefined, Lecture, 2, Multi-access                  ,  Mon Wed, 12:30, 14:00, undefined).
+section(null, undefined, Lecture, 2, Multi-access                  ,  Tue Thu, 9:30, 11:00, undefined).
+section(null, undefined, Lecture-Laboratory, 1, In-Person,  Tue, 16:00, 19:00, undefined).
+section(null, undefined, Waiting List, 1, In-Person,  Tue, 16:00, 19:00, undefined).
+section(Blocked, undefined, Seminar, 2, In-Person,  Mon, 8:30, 10:00, undefined).
+section(null, undefined, Lecture, 1, Online                        ,  ,  ,  , undefined).
+section(null, undefined, Lecture, 1, Online                        ,  ,  ,  , undefined).
+section(null, undefined, Lecture, 1, Online                        ,  ,  ,  , undefined).
+section(Blocked, undefined, Lecture, 2, Online                        ,  ,  ,  , undefined).
+section(Blocked, undefined, Lecture, 2, Online                        ,  ,  ,  , undefined).
+section(Blocked, undefined, Lecture, 2, Online                        ,  ,  ,  , undefined).
+section(Blocked, undefined, Waiting List, 2, Online                        ,  ,  ,  , undefined).
+section(null, undefined, Lecture, 1, Online                        ,  ,  ,  , undefined).
+section(null, undefined, Lecture, 1, Online                        ,  ,  ,  , undefined).
+section(null, undefined, Lecture, 1, Online                        ,  ,  ,  , undefined).
+section(Blocked, undefined, Lecture, 2, Online                        ,  ,  ,  , undefined).
+section(Blocked, undefined, Lecture, 2, Online                        ,  ,  ,  , undefined).
+section(Blocked, undefined, Lecture, 2, Online                        ,  ,  ,  , undefined).
+section(Blocked, undefined, Waiting List, 2, Online                        ,  ,  ,  , undefined).
+section(null, undefined, Lecture, 1, Online                        ,  ,  ,  , undefined).
+section(null, undefined, Lecture, 1, Online                        ,  ,  ,  , undefined).
+section(null, undefined, Lecture, 1, Online                        ,  ,  ,  , undefined).
+section(Blocked, undefined, Lecture, 2, Online                        ,  ,  ,  , undefined).
+section(Blocked, undefined, Lecture, 2, Online                        ,  ,  ,  , undefined).
+section(Blocked, undefined, Lecture, 2, Online                        ,  ,  ,  , undefined).
+section(Blocked, undefined, Waiting List, 2, Online                        ,  ,  ,  , undefined).
+section(null, undefined, Lecture, 1, Online                        ,  ,  ,  , undefined).
+section(null, undefined, Lecture, 1, Online                        ,  ,  ,  , undefined).
+section(null, undefined, Lecture, 1, Online                        ,  ,  ,  , undefined).
+section(Blocked, undefined, Lecture, 2, Online                        ,  ,  ,  , undefined).
+section(Blocked, undefined, Lecture, 2, Online                        ,  ,  ,  , undefined).
+section(Blocked, undefined, Lecture, 2, Online                        ,  ,  ,  , undefined).
+section(Blocked, undefined, Waiting List, 2, Online                        ,  ,  ,  , undefined).
+section(null, undefined, Lecture, 1, Online                        ,  ,  ,  , undefined).
+section(null, undefined, Lecture, 1, Online                        ,  ,  ,  , undefined).
+section(null, undefined, Lecture, 1, Online                        ,  ,  ,  , undefined).
+section(Blocked, undefined, Lecture, 2, Online                        ,  ,  ,  , undefined).
+section(Blocked, undefined, Lecture, 2, Online                        ,  ,  ,  , undefined).
+section(Blocked, undefined, Lecture, 2, Online                        ,  ,  ,  , undefined).
+section(Blocked, undefined, Waiting List, 2, Online                        ,  ,  ,  , undefined).
+section(null, undefined, Lecture, 1, In-Person                     ,  Tue Thu, 11:00, 12:30, undefined).
+section(null, undefined, Lecture, 1, In-Person                     ,  Tue Thu, 14:00, 15:30, undefined).
+section(null, undefined, Lecture, 2, In-Person                     ,  Tue Thu, 14:00, 15:30, undefined).
+section(null, undefined, Lecture, 2, In-Person                     ,  Mon Wed, 15:30, 17:00, undefined).
+section(null, undefined, Lecture, 1, In-Person                     ,  Mon Wed Fri, 15:00, 16:00, undefined).
+section(null, undefined, Lecture, 2, In-Person                     ,  Tue Thu, 11:00, 12:30, undefined).
+section(null, undefined, Lecture, 1, In-Person                     ,  Mon Wed Fri, 13:00, 14:00, undefined).
+section(null, undefined, Lecture, 2, In-Person                     ,  Tue Thu, 12:30, 14:00, undefined).
+section(null, undefined, Lecture, 2, Online                        ,  , , , undefined).
+section(null, undefined, Waiting List, 2, In-Person,  ,  ,  , undefined).
+section(null, undefined, Lecture, 1, In-Person,  Mon Wed, 13:00, 14:00, undefined).
+section(null, undefined, Discussion, 1, In-Person,  Mon, 9:00, 10:00, undefined).
+section(null, undefined, Discussion, 1, In-Person,  Fri, 15:00, 16:00, undefined).
+section(null, undefined, Discussion, 1, In-Person,  Fri, 13:00, 14:00, undefined).
+section(null, undefined, Discussion, 1, In-Person,  Fri, 13:00, 14:00, undefined).
+section(null, undefined, Discussion, 1, In-Person,  Fri, 15:00, 16:00, undefined).
+section(null, undefined, Discussion, 1, In-Person,  Fri, 14:00, 15:00, undefined).
+section(null, undefined, Lecture, 2, In-Person,  Mon Wed, 13:00, 14:00, undefined).
+section(null, undefined, Discussion, 2, In-Person,  Fri, 11:00, 12:00, undefined).
+section(null, undefined, Discussion, 2, In-Person,  Fri, 13:00, 14:00, undefined).
+section(null, undefined, Discussion, 2, In-Person,  Fri, 11:00, 12:00, undefined).
+section(null, undefined, Discussion, 2, In-Person,  Fri, 13:00, 14:00, undefined).
+section(Blocked, undefined, Lecture, 2, Online                        ,  , , , undefined).
+section(null, undefined, Discussion, 2, In-Person,  Fri, 14:00, 15:00, undefined).
+section(null, undefined, Discussion, 2, In-Person,  Fri, 12:00, 13:00, undefined).
+section(Full, undefined, Discussion, 2, In-Person,  Wed, 11:00, 12:00, undefined).
+section(Full, undefined, Discussion, 2, In-Person,  Wed, 14:00, 15:00, undefined).
+section(null, undefined, Discussion, 2, In-Person,  Mon, 15:00, 16:00, undefined).
+section(null, undefined, Discussion, 2, In-Person,  Mon, 9:00, 10:00, undefined).
+section(null, undefined, Waiting List, 1, In-Person,  Mon Wed, 13:00, 14:00, undefined).
+section(null, undefined, Waiting List, 2, In-Person,  Mon Wed, 13:00, 14:00, undefined).
+section(null, undefined, Waiting List, 2, In-Person,  Mon, 8:00, 8:30, undefined).
+section(null, undefined, Lecture, 1, In-Person,  Mon Wed, 12:00, 13:00, undefined).
+section(null, undefined, Discussion, 1, In-Person,  Fri, 12:00, 13:00, undefined).
+section(null, undefined, Waiting List, 1, In-Person,  Mon Wed, 12:00, 13:00, undefined).
+section(null, undefined, Lecture, 1, In-Person,  Mon Wed Fri, 12:00, 13:00, undefined).
+section(null, undefined, Lecture, 2, In-Person,  Mon Wed Fri, 10:00, 11:00, undefined).
+section(null, undefined, Lecture, 1, In-Person,  Tue Thu, 14:00, 15:30, undefined).
+section(null, undefined, Waiting List, 1, In-Person,  Tue Thu, 14:00, 15:30, undefined).
+section(null, undefined, Lecture, 1, In-Person,  Mon Wed Fri, 14:00, 15:00, undefined).
+section(null, undefined, Lecture, 1, In-Person,  Mon Wed Fri, 11:00, 12:00, undefined).
+section(null, undefined, Lecture, 1, In-Person,  Mon Wed Fri, 12:00, 13:00, undefined).
+section(null, undefined, Waiting List, 1, In-Person,  Mon, 8:00, 9:00, undefined).
+section(null, undefined, Lecture, 1, In-Person,  Mon Wed Fri, 13:00, 14:00, undefined).
+section(Cancelled, undefined, Lecture, 2, In-Person,  , , , undefined).
+section(null, undefined, Lecture, 1, In-Person,  Mon Wed Fri, 10:00, 11:00, undefined).
+section(Cancelled, undefined, Lecture, 2, In-Person,  , , , undefined).
+section(null, undefined, Lecture, 2, In-Person,  Mon Wed Fri, 12:00, 13:00, undefined).
+section(null, undefined, Lecture, 2, In-Person,  Tue Thu, 11:00, 12:30, undefined).
+section(Cancelled, undefined, Lecture, 2, In-Person,  , , , undefined).
+section(null, undefined, Lecture, 2, In-Person,  Mon Wed Fri, 9:00, 10:00, undefined).
+section(Cancelled, undefined, Waiting List, 2, In-Person,  , , , undefined).
+section(null, undefined, Lecture, 1, In-Person,  Mon Wed, 17:00, 18:00, undefined).
+section(null, undefined, Tutorial, 1, In-Person,  Fri, 11:00, 12:00, undefined).
+section(null, undefined, Tutorial, 1, In-Person,  Mon, 18:00, 19:00, undefined).
+section(null, undefined, Lecture, 2, In-Person,  Mon Wed, 17:00, 18:00, undefined).
+section(Blocked, undefined, Tutorial, 2, In-Person,  Mon, 11:00, 12:00, undefined).
+section(Blocked, undefined, Tutorial, 2, In-Person,  Wed, 10:00, 11:00, undefined).
+section(null, undefined, Tutorial, 2, In-Person,  Mon, 18:00, 19:00, undefined).
+section(Blocked, undefined, Tutorial, 2, In-Person,  Mon, 13:00, 14:00, undefined).
+section(Blocked, undefined, Tutorial, 2, In-Person,  Fri, 12:00, 13:00, undefined).
+section(null, undefined, Lecture, 1, Online                        ,  , , , undefined).
+section(null, undefined, Lecture, 2, Online                        ,  , , , undefined).
+section(null, undefined, Tutorial, 2, In-Person,  Mon, 16:00, 17:00, undefined).
+section(null, undefined, Waiting List, 1, In-Person,  Mon, 8:00, 9:00, undefined).
+section(null, undefined, Waiting List, 2, In-Person,  Mon, 8:00, 9:00, undefined).
+section(null, undefined, Lecture, 2, In-Person,  Mon Wed Fri, 11:00, 12:00, undefined).
+section(null, undefined, Lecture, 2, In-Person,  Tue Thu, 11:00, 12:30, undefined).
+section(null, undefined, Waiting List, 2, In-Person,  Tue Thu, 11:00, 12:30, undefined).
+section(Cancelled, undefined, Lecture, 1, In-Person,  , , , undefined).
+section(null, undefined, Lecture, 2, In-Person,  Mon Wed Fri, 16:00, 17:00, undefined).
+section(null, undefined, Lecture, 1, In-Person,  Mon Wed Fri, 13:00, 14:00, undefined).
+section(null, undefined, Lecture, 2, In-Person,  Tue Thu, 14:00, 15:30, undefined).
+section(null, undefined, Lecture, 1, In-Person,  Mon Wed Fri, 9:00, 10:00, undefined).
+section(null, undefined, Lecture, 1, In-Person,  Tue Thu, 9:30, 11:00, undefined).
+section(null, undefined, Waiting List, 1, In-Person,  Tue Thu, 9:30, 11:00, undefined).
+section(null, undefined, Lecture, 2, In-Person,  Mon Wed Fri, 11:00, 12:00, undefined).
+section(null, undefined, Lecture, 2, In-Person,  Mon Wed Fri, 13:00, 14:00, undefined).
+section(null, undefined, Seminar, 2, In-Person,  Mon Wed Fri, 11:00, 12:00, undefined).
+section(null, undefined, Lecture, 1, In-Person,  Mon Wed Fri, 12:00, 13:00, undefined).
+section(null, undefined, Lecture, 2, In-Person,  Tue Thu, 9:30, 11:00, undefined).
+section(null, undefined, Lecture, 1, In-Person,  Mon Wed Fri, 10:00, 11:00, undefined).
+section(null, undefined, Waiting List, 1, In-Person,  Mon Wed Fri, 10:00, 11:00, undefined).
+section(null, undefined, Lecture, 2, In-Person,  Mon Wed Fri, 14:00, 15:00, undefined).
+section(Blocked, undefined, Lecture, 1, In-Person,  Tue Thu, 11:00, 12:30, undefined).
+section(Cancelled, undefined, Lecture, 2, In-Person,  , , , undefined).
+section(null, undefined, Lecture, 2, In-Person,  Tue Thu, 12:30, 14:00, undefined).
+section(Cancelled, undefined, Lecture, 1, In-Person,  , , , undefined).
+section(null, undefined, Seminar, 2, In-Person,  Mon Wed Fri, 12:00, 13:00, undefined).
+section(null, undefined, Seminar, 2, In-Person,  Thu, 14:00, 17:00, undefined).
+section(null, undefined, Seminar, 1, In-Person,  Tue, 13:00, 16:00, undefined).
+section(null, undefined, Seminar, 1, In-Person,  Mon Wed Fri, 9:00, 10:00, undefined).
+section(null, undefined, Seminar, 1, In-Person,  Mon Fri, 17:00, 18:30, undefined).
+section(Full, undefined, Seminar, 2, In-Person,  Tue, 14:30, 17:30, undefined).
+section(Blocked, undefined, Directed Studies, 1-2, In-Person,  Mon, 8:00, 9:00, undefined).
+section(Restricted, undefined, Lecture-Laboratory, 1, In-Person,  Tue Thu, 8:00, 9:30, undefined).
+section(Blocked, undefined, Directed Studies, 2, In-Person,  ,  ,  , undefined).
+section(Blocked, undefined, Directed Studies, 1-2, In-Person,  , , , undefined).
+section(Blocked, undefined, Directed Studies, 1-2, In-Person,  ,  ,  , undefined).
+section(null, undefined, Lecture, 2, Online                        ,  Mon Wed, 9:00, 10:00, undefined).
+section(null, undefined, Discussion, 2, Online                        ,  Fri, 10:00, 11:00, undefined).
+section(Full, undefined, Discussion, 2, Online                        ,  Fri, 11:00, 12:00, undefined).
+section(null, undefined, Discussion, 2, Online                        ,  Fri, 12:00, 13:00, undefined).
+section(null, undefined, Discussion, 2, Online                        ,  Fri, 13:00, 14:00, undefined).
+section(null, undefined, Discussion, 2, Online                        ,  Fri, 14:00, 15:00, undefined).
+section(null, undefined, Discussion, 2, Online                        ,  Fri, 15:00, 16:00, undefined).
+section(STT, undefined, Lecture, 1, In-Person,  Tue Thu, 11:00, 12:00, undefined).
+section(null, undefined, Discussion, 1, In-Person,  Thu, 14:00, 15:00, undefined).
+section(Full, undefined, Discussion, 1, In-Person,  Thu, 18:00, 19:00, undefined).
+section(null, undefined, Discussion, 1, In-Person,  Fri, 9:00, 10:00, undefined).
+section(null, undefined, Discussion, 1, In-Person,  Fri, 9:00, 10:00, undefined).
+section(null, undefined, Lecture, 1, In-Person,  Mon Wed, 16:00, 17:00, undefined).
+section(null, undefined, Discussion, 1, In-Person,  Fri, 9:00, 10:00, undefined).
+section(null, undefined, Discussion, 1, In-Person,  Fri, 10:00, 11:00, undefined).
+section(null, undefined, Discussion, 1, In-Person,  Fri, 11:00, 12:00, undefined).
+section(null, undefined, Discussion, 1, In-Person,  Fri, 12:00, 13:00, undefined).
+section(null, undefined, Discussion, 1, In-Person,  Fri, 13:00, 14:00, undefined).
+section(null, undefined, Discussion, 1, In-Person,  Fri, 15:00, 16:00, undefined).
+section(null, undefined, Discussion, 1, In-Person,  Fri, 9:00, 10:00, undefined).
+section(null, undefined, Discussion, 1, In-Person,  Fri, 10:00, 11:00, undefined).
+section(null, undefined, Discussion, 1, In-Person,  Fri, 11:00, 12:00, undefined).
+section(null, undefined, Discussion, 1, In-Person,  Fri, 12:00, 13:00, undefined).
+section(null, undefined, Discussion, 1, In-Person,  Fri, 13:00, 14:00, undefined).
+section(null, undefined, Discussion, 1, In-Person,  Fri, 14:00, 15:00, undefined).
+section(null, undefined, Discussion, 1, In-Person,  Fri, 15:00, 16:00, undefined).
+section(Blocked, undefined, Discussion, 1, In-Person,  Fri, 9:00, 10:00, undefined).
+section(null, undefined, Discussion, 1, In-Person,  Fri, 10:00, 11:00, undefined).
+section(null, undefined, Lecture, 2, In-Person,  Mon Wed, 11:00, 12:00, undefined).
+section(null, undefined, Discussion, 2, In-Person,  Fri, 15:00, 16:00, undefined).
+section(null, undefined, Discussion, 2, In-Person,  Fri, 10:00, 11:00, undefined).
+section(Full, undefined, Discussion, 2, In-Person,  Fri, 11:00, 12:00, undefined).
+section(null, undefined, Discussion, 2, In-Person,  Fri, 12:00, 13:00, undefined).
+section(null, undefined, Discussion, 2, In-Person,  Fri, 13:00, 14:00, undefined).
+section(null, undefined, Discussion, 2, In-Person,  Fri, 14:00, 15:00, undefined).
+section(null, undefined, Lecture, 2, In-Person,  Mon Wed, 14:00, 15:00, undefined).
+section(null, undefined, Discussion, 2, In-Person,  Fri, 9:00, 10:00, undefined).
+section(null, undefined, Discussion, 2, In-Person,  Fri, 10:00, 11:00, undefined).
+section(null, undefined, Discussion, 2, In-Person,  Fri, 11:00, 12:00, undefined).
+section(null, undefined, Discussion, 2, In-Person,  Fri, 12:00, 13:00, undefined).
+section(null, undefined, Discussion, 2, In-Person,  Fri, 13:00, 14:00, undefined).
+section(null, undefined, Discussion, 2, In-Person,  Fri, 15:00, 16:00, undefined).
+section(null, undefined, Lecture, 1, In-Person,  Mon Wed, 9:30, 11:00, undefined).
+section(null, undefined, Lecture, 1, In-Person,  Tue Thu, 11:00, 12:30, undefined).
+section(null, undefined, Lecture, 2, In-Person,  Tue Thu, 9:30, 11:00, undefined).
+section(null, undefined, Lecture, 1, Online                        ,  Tue Thu, 14:00, 15:30, undefined).
+section(null, undefined, Lecture, 1, In-Person,  Tue Thu, 12:30, 14:00, undefined).
+section(null, undefined, Lecture, 2, In-Person,  Tue Thu, 11:00, 12:30, undefined).
+section(null, undefined, Lecture, 1, In-Person,  Tue Thu, 15:30, 17:00, undefined).
+section(null, undefined, Lecture, 1, Online                        ,  Mon Wed, 11:00, 12:30, undefined).
+section(null, undefined, Lecture, 2, In-Person,  Mon Wed, 9:30, 11:00, undefined).
